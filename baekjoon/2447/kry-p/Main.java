c6dba79eec8e8b3fac45f8adb44c1c3f946dc248
kry-p <psd7256@gmail.com>
2021-06-23 12:47:52 +0900

    Add code to Baekjoon's quiz #10158 by kry-p

---
+++ baekjoon/2447/kry-p/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+    private static StringBuilder builder;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        builder = new StringBuilder();
+
+        scanner.close();
+    }
+
+    public static void drawer(int size) {
+    }
+}
